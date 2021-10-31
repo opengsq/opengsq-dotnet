0352cc8d54c31107bfc6bfcee239975b7434f36f
Battlefield Duck <hkdsethomas@gmail.com>
2021-11-01 00:10:35 +0800

    Update Quake3Tests.cs

--- OpenGSQTests/Protocols/Quake3Tests.cs
+++ OpenGSQTests/Protocols/Quake3Tests.cs
@@ -8,18 +8,16 @@ namespace OpenGSQ.Protocols.Tests
     [TestClass()]
     public class Quake3Tests : TestBase
     {
-        // Quake3 - https://www.gametracker.com/search/et/
-        public Quake3 quake3 = new Quake3("108.61.18.110", 27960);
-
         public Quake3Tests() : base(nameof(Quake3Tests))
         {
             _EnableSave = false;
-            _DelayPerTest = 900;
         }
 
         [TestMethod()]
         public void GetInfoTest()
         {
+            // Quake3 - https://www.gametracker.com/search/et/
+            var quake3 = new Quake3("108.61.18.110", 27960);
             var response = quake3.GetInfo();
 
             SaveResult(nameof(GetInfoTest), JsonSerializer.Serialize(response, typeof(Dictionary<string, string>), Options));
@@ -28,6 +26,7 @@ namespace OpenGSQ.Protocols.Tests
         [TestMethod()]
         public void GetStatusTest()
         {
+            var quake3 = new Quake3("108.61.21.93", 27960);
             var response = quake3.GetStatus();
 
             SaveResult(nameof(GetStatusTest), JsonSerializer.Serialize(response, typeof(Quake3.Status), Options));
